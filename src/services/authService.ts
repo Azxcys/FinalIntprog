8c94184ad3f5072bcc6e6ea4fd247925c7179868
Magsayo <henrykenmagsayo@gmail.com>
2025-05-18 18:01:15 +0800

    Render dep Ver 2.3

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://153.92.15.31/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://finalintproggroupf.onrender.com/api';
 
 export interface LoginCredentials {
   username: string;
